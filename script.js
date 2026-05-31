cafc1cb569dd51dae2891e2cbc1bc0e0331a0400
sonu sharma <dexterthebatman00@gmail.com>
2026-05-31 12:34:36 +0530

    added gird change button and clear button

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
 function makeGrid(size) {
-    const screen = document.querySelector(".gird-items")
-
-
+    screen.innerHTML = "";
     for (let i = 0; i < size; i++) {
     const column = document.createElement("div");
     column.classList.add("column")
-
+        screen.appendChild(column);
     for(let j = 0; j < size; j++){
     const row = document.createElement("div");
     row.classList.add("row");
     column.appendChild(row);
         }
-        screen.appendChild(column);
     }
-   
+  
 }
-makeGrid(16);
+
+const screen = document.querySelector(".gird-items")
+const clear = document.querySelector("#delete")
+let isDrawing = false;
+screen.addEventListener('mousedown', () => {
+    isDrawing = true; 
+});
+screen.addEventListener('mouseup', () => {
+    isDrawing = false;
+});
+screen.addEventListener('mouseover' , (event) => {
+    if(isDrawing && event.target.classList.contains("row")) {
+        event.target.style.backgroundColor = "red";
+    }
+});
+const btn = document.querySelector("#btn");
+btn.addEventListener("click" , () => {
+    let newGrid = prompt("enter the size of grids: ")
+    if(newGrid < 100) {
+        makeGrid(Number(newGrid));
+    }else {
+        alert("please enter less than 100 grids!!!")
+    }
+});
+
+clear.addEventListener("click" , () => {
+    let rows = document.querySelectorAll(".row");
+    for(let i = 0; i < rows.length; i++) {
+        rows[i].style.backgroundColor = "";
+    }
+})
